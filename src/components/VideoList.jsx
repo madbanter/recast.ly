ba3a2ed740d1d810101e598cdf59b3edf6d91b69
Adam Brent <adam.brent@gmail.com>
2020-12-08 12:26:07 -0800

    Completes VideoListEntry and VideoListPlayer functional implementations.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,11 +2,11 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    <VideoListEntry video={[props.videos[0]]}/>
-    <VideoListEntry video={[props.videos[1]]}/>
-    <VideoListEntry video={[props.videos[2]]}/>
-    <VideoListEntry video={[props.videos[3]]}/>
-    <VideoListEntry video={[props.videos[4]]}/>
+    {
+      props.videos.map(
+        video => <VideoListEntry video={video}/>
+      )
+    }
   </div>
 );
 
@@ -19,3 +19,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,11 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src={props.video[0].snippet.thumbnails.default.url} alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{props.video[0].snippet.title}</div>
-      <div className="video-list-entry-detail">{props.video[0].snippet.description}</div>
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
 );
