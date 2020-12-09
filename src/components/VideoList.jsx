f41dcf81fb00384d466020d196e52e7233f33b5e
Adam Brent <adam.brent@gmail.com>
2020-12-08 20:21:20 -0800

    Refactor to ensure tests are passing.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,15 @@
 import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = (props) => (
-  <div className="video-list">
+var VideoList = (props) => {
+  // console.log(props);
+  return (<div className="video-list">
     {
       props.videos.map(
         video => <VideoListEntry video={video} updateCurrentVideo={props.updateCurrentVideo} />
       )
     }
-  </div>
-);
+  </div>);
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
